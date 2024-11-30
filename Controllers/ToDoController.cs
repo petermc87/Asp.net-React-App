0a62fa4222af22cd9ecfc455407bdeb1791d01eb
Peter McGibney <peter.mcgibney@gmail.com>
2024-11-29 22:44:43 -0500

    Initial commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        // Injecting the the configuration settings to the toDoController class
+        // using dependency injection.
+        private IConfiguration _configuration;
+
+        public ToDoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = "select * from dbo.notes";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("todoAppDbCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
